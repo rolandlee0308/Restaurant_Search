1e7cd6b97a46bb683bfa2ed76b4afb6e30025035
Roland Lee <rolandlee500@gmail.com>
2022-09-12 09:46:21 -0700

    Setting up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb/hooks'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    // Disabled because react native uses js, not jsx
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    // Disabled because style prop can be object or string
+    'react/style-prop-object': 'off',
+  },
+};
